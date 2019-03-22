96c0001cba7b18d5204ee3824372f94c05051fff
CHION Valentin <valentin.chion@soprasteria.com>
2019-03-22 13:57:54 +0100

    Import images to project

--- assets/imageImport.js
+++ assets/imageImport.js
@@ -1,4 +1,7 @@
 export const images = {
   sopraSteriaLogo: require('./logo-sopra-steria.png'),
-  auchanLogo: require('./logo-auchan.png')
+  auchanLogo: require('./logo-auchan.png'),
+  talendIDE: require('./IDETalend.png'),
+  etlDiagram: require('./etlDiagram.png'),
+  mdmDiagram: require('./mdmDiagram.png')
 };
